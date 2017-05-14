b8d33ed7ac5e2275aa08743b2c4b0731c7c3fe39
houshuai0816 <houshuai0816@126.com>
2017-05-14 21:06:46 +0800

    Initial commit

---
+++ app/src/main/java/com/houshuai/text/MainActivity.java
@@ -0,0 +1,15 @@
+package com.houshuai.text;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.houshuai.text.googlefirebase.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
